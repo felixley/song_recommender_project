ce4dac2655e1ddc73d0ccbe06fc6fb3b13a170e3
Felix Ley <69302556+felixley@users.noreply.github.com>
2021-02-26 12:39:21 +0100

    Update spotify_client.py

--- spotify_client.py
+++ spotify_client.py
@@ -1,2 +1,5 @@
+# you need to sign up for the usage of spotify api 
+# just go to https://developer.spotify.com/dashboard/login
+
 client_id= "insert your client id"
 client_secret= "insert your client secret"
